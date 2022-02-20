fe6a629316fcfa98984145b1bcfbfbb31a542a6a
muzigae <91370495+muzigae@users.noreply.github.com>
2022-02-21 02:25:56 +0900

    B - Card Game for Three (ABC Edit)
    https://atcoder.jp/contests/abc045/tasks/abc045_b

---
+++ AtCoder_Beginner_Contest/045/B.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <queue>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    vector<queue<int> > C(3);
+    for (int i = 0; i < 3; i++) {
+        string deck; cin >> deck;
+        for (auto card : deck) C[i].push(card - 'a');
+    }
+
+    int player = 0;
+    while (1) {
+        if (C[player].empty()) return !(cout << char(player + 'A'));
+        int next_player = C[player].front();
+        C[player].pop(); player = next_player;
+    }
+    return 0;
+}
