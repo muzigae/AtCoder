967677d8dfb1da0aafe6509de73659a94b24bf98
muzigae <91370495+muzigae@users.noreply.github.com>
2022-02-21 01:48:18 +0900

    A - Trapezoids
    https://atcoder.jp/contests/abc045/tasks/abc045_a

---
+++ AtCoder_Beginner_Contest/045/A.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int a, b, h; cin >> a >> b >> h;
+    cout << (a + b) * h / 2;
+    return 0;
+}
